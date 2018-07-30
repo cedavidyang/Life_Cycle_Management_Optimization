1dd9d68b9b3c861e7deb5b80331da6360c4700cb
David Yang <ynyang1988@yahoo.com>
2018-07-30 11:41:00 -0400

    use EV theory for annual reliability

--- management/component.py
+++ management/component.py
@@ -199,17 +199,16 @@ class Component(object):
         R = stats.lognorm(np.sqrt(np.log(1+rcov**2)), scale=rmean/np.sqrt(1+rcov**2))
         # live load array
         if 'f' in self.comp_type.lower():
-            loc=M_LLIM_MEAN
-            scale=M_LLIM_MEAN*M_LLIM_COV
+            nannual = LL_ARRIVAL_RATE
+            slrv = stats.norm(loc=M_LLIM_MEAN, scale=M_LLIM_MEAN*M_LLIM_COV)
         elif 's' in self.comp_type.lower():
-            loc=V_LLIM_MEAN
-            scale=V_LLIM_MEAN*V_LLIM_COV
+            nannual = LL_ARRIVAL_RATE
+            slrv = stats.norm(loc=V_LLIM_MEAN, scale=V_LLIM_MEAN*V_LLIM_COV)
         elif 'd' in self.comp_type.lower():
-            loc= M_LLIM_DECK_MEAN
-            scale=M_LLIM_DECK_MEAN*M_LLIM_DECK_COV
-        # loc=mean-np.sqrt(6)*stdv/np.pi*np.euler_gamma,
-        # scale=np.sqrt(6)*stdv/np.pi
-	mean = loc+scale*np.euler_gamma
+            nannual = LL_ARRIVAL_RATE_DECK
+            slrv = stats.norm(loc=M_LLIM_DECK_MEAN, scale=M_LLIM_DECK_MEAN*M_LLIM_DECK_COV)
+        loc = slrv.ppf(1-1./nannual)
+        scale = 1./ (nannual*slrv.pdf(loc))
         SL = stats.gumbel_r(loc=loc,scale=scale)
         rvs = [R, SL]
         corr = np.eye(2)
